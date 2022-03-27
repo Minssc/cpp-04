c038e8d513767a52d0b94c48c3bab13014dd1644
minsunki <minsunki@student.42seoul.kr>
2022-03-28 00:29:33 +0900

    ex03 touchup, more main.cpp content

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 19:00:24 by minsunki          #+#    #+#             */
-/*   Updated: 2022/03/27 19:40:13 by minsunki         ###   ########seoul.kr  */
+/*   Updated: 2022/03/28 00:28:43 by minsunki         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,18 @@ int main()
 	delete me;
 	delete src;
 	
+	src = new MateriaSource();
+	src->learnMateria(new Ice());
+
+	me = new Character("me");
+	me->equip(src->createMateria("ice"));
+	me->equip(src->createMateria("whatisthismateria"));
+
+	bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(4, *bob);
 	return 0;
 }
